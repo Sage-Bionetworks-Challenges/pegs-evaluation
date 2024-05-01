db1a0183d6934a93fb897814eaa9d608242d00d6
verena <9377970+vpchung@users.noreply.github.com>
2024-05-01 10:06:17 -0700

    use score_* annotations

--- score.py
+++ score.py
@@ -10,19 +10,15 @@ import argparse
 import json
 
 import pandas as pd
-from sklearn.metrics import (roc_auc_score,
-                             average_precision_score)
+from sklearn.metrics import roc_auc_score, average_precision_score
 
 
 def get_args():
     """Set up command-line interface and get arguments."""
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--predictions_file",
-                        type=str, required=True)
-    parser.add_argument("-g", "--goldstandard_file",
-                        type=str, required=True)
-    parser.add_argument("-o", "--output",
-                        type=str, default="results.json")
+    parser.add_argument("-p", "--predictions_file", type=str, required=True)
+    parser.add_argument("-g", "--goldstandard_file", type=str, required=True)
+    parser.add_argument("-o", "--output", type=str, default="results.json")
     return parser.parse_args()
 
 
@@ -33,7 +29,7 @@ def score(gold, gold_col, pred, pred_col):
     roc = roc_auc_score(gold[gold_col], pred[pred_col])
     pr = average_precision_score(gold[gold_col], pred[pred_col])
 
-    return {'auc_roc': roc, 'auprc': pr}
+    return {"auc_roc": roc, "auprc": pr}
 
 
 def main():
@@ -53,10 +49,15 @@ def main():
         status = "INVALID"
 
     res |= {
-        "validation_status": "SCORED",
-        **scores
+        "score_status": status,
+        "score_errors": (
+            ""
+            if status == "SCORED"
+            else "Prediction file invalid; cannot be scored."
+        ),
+        ** scores
     }
-    with open(args.output, "w") as out:
+    with open(args.output, "w", encoding="utf-8") as out:
         out.write(json.dumps(res))
 
 
