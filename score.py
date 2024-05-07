f5d5a3c8c0aa13d3140434251df11c490fa059a0
Jenny Medina <medina.stsci@gmail.com>
2024-05-06 22:28:58 -0400

    Update score.py

--- score.py
+++ score.py
@@ -8,16 +8,18 @@ Metrics to return:
 
 import argparse
 import json
+import os
 
 import pandas as pd
 from sklearn.metrics import roc_auc_score, average_precision_score
 
+from glob import glob
 
 def get_args():
     """Set up command-line interface and get arguments."""
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--predictions_file", type=str, required=True)
-    parser.add_argument("-g", "--goldstandard_file", type=str, required=True)
+    parser.add_argument("-g", "--goldstandard_folder", type=str, required=True)
     parser.add_argument("-o", "--output", type=str, default="results.json")
     return parser.parse_args()
 
@@ -31,6 +33,15 @@ def score(gold, gold_col, pred, pred_col):
     return {"auc_roc": roc, "auprc": pr}
 
 
+def extract_gs_file(folder):
+    """Extract gold standard file from folder."""
+    files = glob(os.path.join(folder, "*"))
+    if len(files) != 1:
+        raise ValueError(f"Expected exactly one gold standard file in folder. Got {len(files)}. Exiting.")
+
+    return files[0]
+
+
 def main():
     """Main function."""
     args = get_args()
@@ -38,9 +49,11 @@ def main():
     with open(args.output, encoding="utf-8") as out:
         res = json.load(out)
 
+    gold_file = extract_gs_file(args.goldstandard_folder)
+
     if res.get("validation_status") == "VALIDATED":
         pred = pd.read_csv(args.predictions_file)
-        gold = pd.read_csv(args.goldstandard_file)
+        gold = pd.read_csv(gold_file)
         scores = score(gold, "disease", pred, "disease_probability")
         status = "SCORED"
     else:
